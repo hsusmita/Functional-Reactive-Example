e13bf891f25563e41868f5f7726d6638a452fed7
Pulkit Vaid <pulkitvaid@Pulkits-MacBook-Pro.local>
2017-04-27 16:00:34 +0530

    Validator Property and Action Example Added

---
+++ Functional-Reactive-Example/ValidatorAndActionViewController/ValidatorAndActionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ValidatorAndActionViewController.swift
+//  Functional-Reactive-Example
+//
+//  Created by Pulkit Vaid on 27/04/17.
+//  Copyright © 2017 hsusmita. All rights reserved.
+//
+
+import UIKit
+
+class ValidatorAndActionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
