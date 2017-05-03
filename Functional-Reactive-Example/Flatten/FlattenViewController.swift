9cc5979d02ee0ed39e30452ee74d7d63d7cacca6
Pulkit Vaid <pulkitvaid@Pulkits-MacBook-Pro.local>
2017-05-03 15:23:36 +0530

    Flatten vc and view model

--- Functional-Reactive-Example/Flatten/FlattenViewController.swift
+++ Functional-Reactive-Example/Flatten/FlattenViewController.swift
@@ -7,29 +7,84 @@
 //
 
 import UIKit
+import ReactiveCocoa
+import ReactiveSwift
+import Result
 
-class FlattenViewController: UIViewController {
+class FlattenViewController: UIViewController, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	@IBOutlet weak var buttonMen: UIButton!
+	@IBOutlet weak var buttonWomen: UIButton!
+	@IBOutlet weak var buttomKids: UIButton!
 
-        // Do any additional setup after loading the view.
-    }
+	@IBOutlet weak var tableViewClothes: UITableView!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+	let viewModel = FlattenViewModel()
+	let disposable = CompositeDisposable()
 
-    /*
-    // MARK: - Navigation
+	override func viewDidLoad() {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+		super.viewDidLoad()
+		setupTableView()
+//		setupEventObserver()
 
+		disposable += buttonMen.reactive.controlEvents(.touchUpInside).observe { [unowned self]
+			_ in
+			self.viewModel.generateEvent(category: .men)
+		}
+
+		disposable += buttonWomen.reactive.controlEvents(.touchUpInside).observe { [unowned self]
+			_ in
+			self.viewModel.generateEvent(category: .women)
+		}
+
+		disposable += buttomKids.reactive.controlEvents(.touchUpInside).observe { [unowned self]
+			_ in
+			self.viewModel.generateEvent(category: .kids)
+		}
+	}
+
+	func setupTableView() {
+		//		tableView.reactive.reloadData <~ data.output.signal.map { _ in return () }
+		//		tableView.reactive.reloadData <~ ds.producer.map{ _ in return ()}
+		//		data.input.send(value: ["asd", "asd"])
+		tableViewClothes.dataSource = self
+		tableViewClothes.reactive.reloadData <~ viewModel.ds.producer.map{ _ in return () }
+	}
+
+//	func setupEventObserver() {
+//		viewModel.eventSignal.output.observeValues {
+//			(event) in
+//			print("Event Recived : \(event)")
+//		}
+//	}
+
+	//MARK: - UITableViewDataSource
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return viewModel.numberofRowsInSection(section: section)
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+		let model = viewModel.tableViewModelforIndexPath(indexPath: indexPath)
+		cell.textLabel?.text = model?.itemName
+		cell.detailTextLabel?.text = "\(model?.price ?? 0)"
+
+		return cell
+	}
+
+	deinit {
+		disposable.dispose()
+	}
+}
+
+
+extension FlattenViewController: Routable {
+	static var storyboardId: String {
+		return "FlattenViewController"
+	}
+
+	static var storyboardName: String {
+		return "Main"
+	}
 }
--- Functional-Reactive-Example/Flatten/FlattenViewModel.swift
+++ Functional-Reactive-Example/Flatten/FlattenViewModel.swift
@@ -7,3 +7,162 @@
 //
 
 import Foundation
+import ReactiveCocoa
+import ReactiveSwift
+import Result
+
+class TableViewDataModel {
+	var itemName: String = ""
+	var price: Float = 0.0
+}
+extension TableViewDataModel {
+	var type: String {
+		return "TableViewDataModel"
+	}
+}
+
+enum MenSizes: String {
+	case s = "36"
+	case m =  "38"
+	case l =  "40"
+	case xl =  "42"
+}
+
+class MenDataModel: TableViewDataModel {
+	var size: MenSizes = .m
+
+	init(name: String, price: Float, size: MenSizes) {
+		super.init()
+		self.itemName = name
+		self.price = price
+		self.size = size
+	}
+}
+
+enum WomenSizes: String {
+	case s = "30"
+	case m =  "32"
+	case l =  "34"
+	case xl =  "36"
+}
+class WomenDataModel: TableViewDataModel {
+	var size: WomenSizes = .m
+	init(name: String, price: Float, size: WomenSizes) {
+		super.init()
+		self.itemName = name
+		self.price = price
+		self.size = size
+	}
+}
+
+enum KidSizes: String {
+	case s = "1-2 years"
+	case m = "3-4 years"
+	case l = "4-5 years"
+	case xl = "5-6 years"
+}
+class KidsDataModel: TableViewDataModel {
+	var size: KidSizes = .m
+	init(name: String, price: Float, size: KidSizes) {
+		super.init()
+		self.itemName = name
+		self.price = price
+		self.size = size
+	}
+}
+
+enum ClothingCategory: Int {
+	case men, women, kids
+}
+
+class FlattenViewModel {
+
+	let datasourceMen: MutableProperty<[TableViewDataModel]?> = MutableProperty<[TableViewDataModel]?>(nil)
+	let datasourceWomen: MutableProperty<[TableViewDataModel]?> = MutableProperty<[TableViewDataModel]?>(nil)
+	let datasourceKids: MutableProperty<[TableViewDataModel]?> = MutableProperty<[TableViewDataModel]?>(nil)
+
+	var ds: SignalProducer<[TableViewDataModel], NoError>!
+	var currentSelection: ClothingCategory = .men
+	let clothingSignal = Signal<ClothingCategory, NoError>.pipe()
+
+	init() {
+		ds = SignalProducer(
+			[datasourceMen.producer.skipNil(),
+			 datasourceWomen.producer.skipNil(),
+			 datasourceKids.producer.skipNil()
+			])
+			.flatten(.merge)
+	}
+
+
+	func setupDataSource() {
+
+		switch currentSelection {
+		case .men:
+			datasourceMen.value = [
+				MenDataModel(name: "Printed T Shirt", price: 499, size: .s),
+				MenDataModel(name: "Shorts", price: 599, size: .m),
+				MenDataModel(name: "Shirt T Shirt", price: 699, size: .l),
+				MenDataModel(name: "Blazer", price: 799, size: .xl)
+			]
+
+		case .women:
+			datasourceWomen.value = [
+				WomenDataModel(name: "Tank Top", price: 499, size: .s),
+				WomenDataModel(name: "Printed Dress", price: 599, size: .m),
+				WomenDataModel(name: "Crop Top", price: 699, size: .l),
+				WomenDataModel(name: "Black Dress", price: 799, size: .xl)
+			]
+		case .kids:
+			datasourceKids.value = [
+				KidsDataModel(name: "Iron Man T Shirt Boys", price: 499, size: .s),
+				KidsDataModel(name: "Batman T Shirt Boys", price: 499, size: .m),
+				KidsDataModel(name: "Swat Kats T Shirt Boys", price: 499, size: .l),
+				KidsDataModel(name: "DragonBallZ T Shirt Boys", price: 499, size: .xl)
+
+			]
+		}
+	}
+
+	//MARK: - Event Generation
+	func generateEvent(category: ClothingCategory, after delay: Int = 0) {
+		switch category {
+		case .men:
+			clothingSignal.input.send(value: .men)
+		case .women:
+			clothingSignal.input.send(value: .women)
+		case .kids:
+			clothingSignal.input.send(value: .kids)
+		}
+		currentSelection = category
+		setupDataSource()
+	}
+
+	//MARK: - TableViewHelper
+	func numberofRowsInSection(section: Int) -> Int {
+		switch currentSelection {
+		case .men:
+			return datasourceMen.value?.count ?? 0
+		case .women:
+			return datasourceWomen.value?.count ?? 0
+		case .kids:
+			return datasourceKids.value?.count ?? 0
+		}
+	}
+
+	func tableViewModelforIndexPath(indexPath: IndexPath) -> TableViewDataModel? {
+		switch currentSelection {
+		case .men:
+			guard let itemArr = datasourceMen.value as? [MenDataModel] else { return nil }
+			return itemArr[indexPath.row]
+		case .women:
+			guard let itemArr = datasourceWomen.value as? [WomenDataModel] else { return nil }
+			return itemArr[indexPath.row]
+		case .kids:
+			guard let itemArr = datasourceKids.value as? [KidsDataModel] else { return nil }
+			return itemArr[indexPath.row]
+		}
+	}
+
+}
+
